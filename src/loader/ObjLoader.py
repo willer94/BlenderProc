100413e2b41ed085a55d786a1fefb50176004231
willer94 <wangzitju@gmail.com>
2019-12-10 09:15:24 +0800

    add axis direction selection for .obj import

--- src/loader/ObjLoader.py
+++ src/loader/ObjLoader.py
@@ -20,4 +20,9 @@ class ObjLoader(Module):
         Module.__init__(self, config)
 
     def run(self):
-        bpy.ops.import_scene.obj(filepath=Utility.resolve_path(self.config.get_string("path")))
+        # the default value for forward and axis_up is '-Z' and 'Y'
+        # but the rbot model is saved from meshlab, and y axis should be turned
+        bpy.ops.import_scene.obj(filepath     = Utility.resolve_path(self.config.get_string("path")),
+                                 axis_forward = self.config.get_string("axis_forward", "-Z"),
+                                 axis_up      = self.config.get_string("axis_up", "Y") 
+                                 )
